7435aa1725d03565ba0439b5e90e2b743d86195d
WOOJIN SON <36991763+swj9707@users.noreply.github.com>
2022-08-07 20:11:40 +0900

    feature/businessLogic businessLogin Setup

--- src/main/java/com/example/moviereviewdemo/service/MovieService.java
+++ src/main/java/com/example/moviereviewdemo/service/MovieService.java
@@ -1,8 +1,74 @@
 package com.example.moviereviewdemo.service;
 
 import com.example.moviereviewdemo.dto.MovieDTO;
+import com.example.moviereviewdemo.dto.MovieImageDTO;
+import com.example.moviereviewdemo.dto.PageRequestDTO;
+import com.example.moviereviewdemo.dto.PageResultDTO;
+import com.example.moviereviewdemo.entity.Movie;
+import com.example.moviereviewdemo.entity.MovieImage;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public interface MovieService {
 
     Long register(MovieDTO movieDTO);
+
+    PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO requestDTO);
+
+    MovieDTO getMovie(Long mno);
+
+    default MovieDTO entitiesToDTO(Movie movie, List<MovieImage> movieImages, Double avg, Long reviewCnt){
+        MovieDTO movieDTO = MovieDTO.builder()
+                .mno(movie.getMno())
+                .title(movie.getTitle())
+                .regDate(movie.getRegDate())
+                .modDate(movie.getModDate())
+                .build();
+
+        List<MovieImageDTO> movieImageDTOList = movieImages.stream().map(
+                movieImage -> {
+                    return MovieImageDTO.builder().imgName(movieImage.getImgName())
+                            .path(movieImage.getPath())
+                            .uuid(movieImage.getUuid())
+                            .build();
+                }).collect(Collectors.toList());
+
+        movieDTO.setImageDTOList(movieImageDTOList);
+        movieDTO.setAvg(avg);
+        movieDTO.setReviewCnt(reviewCnt.intValue());
+
+        return movieDTO;
+    }
+
+
+    default Map<String, Object> dtoToEntity(MovieDTO movieDTO){
+        Map<String ,Object> entityMap = new HashMap<>();
+
+        Movie movie = Movie.builder()
+                .mno(movieDTO.getMno())
+                .title(movieDTO.getTitle())
+                .build();
+        entityMap.put("movie", movie);
+        List<MovieImageDTO> imageDTOList = movieDTO.getImageDTOList();
+
+        if(imageDTOList != null && imageDTOList.size() > 0){
+            List<MovieImage> movieImageList = imageDTOList.stream().map(movieImageDTO -> {
+                MovieImage movieImage = MovieImage.builder()
+                        .path(movieImageDTO.getPath())
+                        .imgName(movieImageDTO.getImgName())
+                        .uuid(movieImageDTO.getUuid())
+                        .movie(movie)
+                        .build();
+                return movieImage;
+            }).collect(Collectors.toList());
+
+            entityMap.put("imgList", movieImageList);
+        }
+        return entityMap;
+    }
+
 }
